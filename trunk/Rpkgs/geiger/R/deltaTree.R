41becdd20ea08fdde0b3762e5ddf7679b000315a
hlapp (Hilmar Lapp) <hlapp@drycafe.net>
2007-12-10 14:32:55 +0000

    Initial import of the CRAN version of Geiger (by Luke Harmon).
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/phyloc@13 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ trunk/Rpkgs/geiger/R/deltaTree.R
@@ -0,0 +1,149 @@
+############################################################
+###############TREE TRANSFORMATIONS#########################
+############################################################
+
+###LAMBDA###
+lambdaTree <- function(phy, lambda)
+{
+	original.rtt <- max(branching.times(phy))
+	ltree <- phy
+	ltree.old <- new2old.phylo(phy) #needed due to change in ape tree format
+	ltree$edge.length <- ltree$edge.length * lambda #shortens internal branches in proportion to lambda
+	t <- original.rtt * (1-lambda) #needed to rescale tree back to its original root-to-tip length
+	which(ltree.old$edge[,2] > 0) -> terminal.edges #identifies termal edges to be extended for recovery of original rtt length; based on old tree format in which nodes are coded as negative values
+	ltree$edge.length[terminal.edges] + t -> ltree$edge.length[terminal.edges] #extends terminal edges
+	return(ltree)
+}
+
+###DELTA###
+deltaTree<-function(phy, delta, rescale=F)
+{
+	tmp<-as.numeric(phy$edge)
+	times<-branching.times(phy)	
+	max(times)->original.rtt.length
+	times=max(times)-times
+	max(times)
+	res<-phy
+	for(i in 1:length(phy$edge.length)) {
+		bl<-phy$edge.length[i]
+		age=times[which(names(times)==phy$edge[i,1])]
+		res$edge.length[i]<-(age+bl)^delta-age^delta
+	}
+	if(rescale==T) res<-rescaleTree(res,original.rtt.length)
+	res
+}
+
+###TWORATE#####
+tworateTree<-function(phy, breakPoint, endRate) 
+{
+	times<-branching.times(phy)	
+	for(i in 1:length(phy$edge.length)) {
+		bl<-phy$edge.length[i]
+		age=times[which(names(times)==phy$edge[i,1])] #gets tip to node length
+		if((age-bl)<breakPoint) #identifies branches that are on the tip side of the break-point (i.e., young)
+			phy$edge.length[i]<-(age-min(age, breakPoint))*1+(min(age, breakPoint)-(age-bl))*endRate #If an edge is entirely to the right of the breakpoint it is simply multipled by f.  However, if the edge extends across the breakpoint, we want to leave the part to the left of the BP unchanged (this is the first part of this equation, which multiplies this part by 1).  We then want to multiply the part of the edge that is to the right of the BP by f (this is the second part of this line).
+		}
+	phy
+}
+
+###LINEAR CHANGE###
+linearchangeTree<-function(phy, endRate=NULL, slope=NULL)
+{
+	
+    if(is.null(slope)&&is.null(endRate))
+    	stop("Must supply either endRate or slope")
+    	
+	times<-branching.times(phy)
+	names(times)<-(as.numeric(names(times)))
+	rootdepth=max(times)
+	
+	
+	
+	if(is.null(slope)) {
+		slope=(endRate-1)/rootdepth
+		}
+	
+
+	getrate<-function(begin, end)
+	{
+		br<-1+begin*slope
+		er<-1+end*slope
+		if(br>0 & er>0) return((br+er)/2)
+		if(br<0 & er<0) return(0)
+		ii<- -1/slope
+		return(br*(ii-begin)/(2*(end-begin)))
+	}
+
+	for(i in 1:length(phy$edge.length)) {
+		bl<-phy$edge.length[i]
+		age=rootdepth-times[which(names(times)==phy$edge[i,1])]
+		end=age+bl
+	
+		phy$edge.length[i]<-phy$edge.length[i]*getrate(age, end)
+
+		
+
+	}
+	phy	
+}
+
+###RESCALING###
+##
+rescaleTree<-function(phy, totalDepth)
+{
+	d<-max(branching.times(phy))
+	phy$edge.length<-(phy$edge.length/d)*totalDepth
+	phy
+}
+
+exponentialchangeTree<-function (phy, endRate=NULL, a=NULL) 
+{
+	
+
+    if(is.null(a)&&is.null(endRate))
+    	stop("Must supply either endRate or a")
+    	
+   	times <- branching.times(phy)
+    d<-max(times)
+    if(is.null(a))
+ 	   a<-log(endRate)/d
+ 	if(a==0) return(phy)   
+    names(times) <- (as.numeric(names(times)))
+    for (i in 1:length(phy$edge.length)) {
+        bl <- phy$edge.length[i]
+        age = times[which(names(times) == phy$edge[i, 1])]
+        t1 = max(times) - age
+        t2 = t1+bl
+        phy$edge.length[i] = (exp(a*t2)-exp(a*t1))/(a)
+    }
+    phy
+}
+
+speciationalTree<-function(phy)
+{
+	phy$edge.length[]=1
+	phy	
+}
+
+ouTree<-function(phy, alpha) {
+	times <- branching.times(phy)
+    names(times) <- (as.numeric(names(times)))
+    Tmax<-times[1]
+    phy2<-phy
+    for (i in 1:length(phy$edge.length)) {
+        bl <- phy$edge.length[i]
+        age = times[which(names(times) == phy$edge[i, 1])]
+        t1 = max(times) - age
+        t2 = t1+bl
+        phy2$edge.length[i] = (1/(2*alpha))*exp(-2*alpha * (Tmax-t2)) * (1 - exp(-2 * alpha * t2)) - 
+        						(1/(2*alpha))*exp(-2*alpha * (Tmax-t1)) * (1 - exp(-2 * alpha * t1))
+    }
+    phy2
+	
+}
+
+kappaTree<-function(phy, kappa)
+{
+	phy$edge.length<-phy$edge.length^kappa
+	return(phy)	
+}
---
+++ trunk/Rpkgs/geiger/R/fit.continuous.R
@@ -0,0 +1,309 @@
+`fit.continuous` <-
+function(phy, data, data.names=NULL, lambda=FALSE, kappa=FALSE, delta=FALSE, ou=FALSE, eb=FALSE, bounds=NULL,  meserr=NULL)
+{
+	
+	# sort is T because sub-functions assume data are in
+	# this particular order
+	
+	td<-treedata(phy, data, data.names, sort=T)
+
+	ntax=length(td$phy$tip.label)
+
+	if(is.null(meserr)) {
+		me=td$data
+		me[]=0
+		meserr=me	
+	} else if(length(meserr)==1) {
+		me=td$data
+		me[]=meserr
+		meserr=me
+	} else if(is.vector(meserr)) {
+		if(!is.null(names(meserr))) {
+			o<-match(rownames(td$data), names(meserr))
+			if(length(o)!=ntax) stop("meserr is missing some taxa from the tree")
+			meserr<-as.matrix(meserr[o,])
+		} else {
+			if(length(meserr)!=ntax) stop("No taxon names in meserr, and the number of taxa does not match the tree")
+			me<-td$data
+			me[]=meserr
+			meserr=me
+		}
+	} else {
+		if(!is.null(rownames(meserr))) {
+			o<-match(rownames(td$data), rownames(meserr))
+			meserr=meserr[o,]
+		} else {
+			if(sum(dim(meserr)!=dim(td$data))!=0)
+				stop("No taxon names in meserr, and the number of taxa does not match the tree")
+			print("No names in meserr; assuming that taxa are in the same order as tree")	
+		}
+	}
+
+	#--------------------------------
+    #---    PREPARE DATA LIST     ---
+    #--------------------------------
+	ds			<- list()
+   		ds$tree 		<- td$phy          # TIP data 
+    #--------------------------------
+    #--- SET MODEL SPECIFICATIONS ---
+    #--------------------------------
+    model<-c(lambda, kappa, delta, ou, eb)
+	names(model)<- c("lambda", "kappa", "delta", "ou", "eb")
+	if (sum(model) > 1)
+		stop("Currently, lambda, kappa, delta, ou, and eb can only be fit one at a time")
+    #-----------------------------
+    #---  SET PARAMETER BOUNDS ---
+    #-----------------------------
+    #---- DEFAULT BOUNDS
+    bounds.default			 <- matrix(c(0.00000001, 20, 0.0000001,1, 0.000001, 1, 0.00001, 5, 0.0000001, 50, -3, 0), nrow=6, ncol=2, byrow=TRUE)
+    rownames(bounds.default) <- c("beta", "lambda", "kappa", "delta", "alpha", "a");
+    colnames(bounds.default) <- c("min", "max")
+
+ 	#---- USER DEFINED PARAMETER BOUNDS
+ 	if (is.null(bounds)) {
+ 		bounds <- bounds.default       # USE DEFAULTS
+ 	}else{
+ 		if (class(bounds)!="list"){
+ 			stop("Please specify user defined parameter bounds as a list()")
+ 		}else{
+ 			specified   <- !c(is.null(bounds$beta), is.null(bounds$lambda), 
+ 							  is.null(bounds$kappa), is.null(bounds$delta),  is.null(bounds$alpha), is.null(bounds$r)
+ 							  )
+ 			bounds.user <- matrix(c(bounds$beta, bounds$lambda, bounds$kappa, bounds$delta, bounds$alpha, bounds$endRate), 
+ 								  nrow=sum(specified), ncol=2, byrow=TRUE
+ 								  )
+ 			rownames(bounds.user) <- c("beta", "lambda", "kappa", "delta", "alpha", "endRate")[specified]
+   	 		colnames(bounds.user) <- c("min", "max")
+  
+   	 		#----  SET FINAL SEARCH BOUNDS
+ 			bounds <- bounds.default
+ 			bounds[specified,] <- bounds.user     # Final Bounds
+   		} # END if list
+   	}  # END user bound if loop
+   	#--------------------------------
+    #---   APPEND MODEL SETTINGS  ---
+    #--------------------------------
+  	ds$bounds <- data.frame(t(bounds))
+
+  	ds$model  <- model
+  	#--------------------------------
+    #---        FIT MODEL         ---
+    #--------------------------------
+    result<-list()
+    for(i in 1:ncol(td$data)) {
+    	ds$data=td$data[,i]
+    	ds$meserr=meserr[,i]
+  		result[[i]]<-fitContinuousModel(ds, print=print)
+  	}
+  	result
+}
+
+
+`fitContinuousModel` <-
+function(ds, print=TRUE)
+{
+	bounds 	<- ds$bounds
+	model 	<- ds$model
+	np 		<- sum(model)
+	n 		<- length(ds$data)
+	#--- INITIALIZE RESULTS MATRIX --
+    results <- numeric(2+np)
+    names(results)<- c("mu", "beta", c("lambda", "kappa", "delta", "alpha", "endRate")[model])
+	#----- MINIMIZE NEGATIVE LOG LIKELIHOOD
+	
+	beta.start<-var(ds$data)/max(branching.times(ds$tree))
+	theta.start <-c(log(beta.start),c(log(0.5), log(0.5), log(0.5), log(0.1), 0.0001)[model])     # Starting point for profile search
+	
+	lower=bounds[1,][c(1, model)==1]
+	upper=bounds[2,][c(1, model)==1]
+
+	out         <- NULL
+	
+	y			<- ds$data				# TIP data
+	tree		<- ds$tree			# Tree
+	meserr		<- ds$meserr
+	n			<- length(y)
+	
+	if (sum(names(model)==c("lambda", "kappa", "delta", "ou", "eb"))!=5)
+		stop("Error with \"model\" internal paramter")
+
+
+	#----------------------------------
+	#-----       DEFAULT FIT      -----
+	#----------------------------------
+	if (sum(model)==0) {
+		
+		lower=log(lower)
+		upper=log(upper)
+
+		vcv<-vcv.phylo(tree)
+
+		
+		foo<-function(x) {
+			vv<-exp(x)*vcv
+			diag(vv)<-diag(vv)+meserr^2
+			mu<-phylogMean(vv, y)
+			mu<-rep(mu, n)
+			-dmvnorm(y, mu, vv, log=T)
+		}
+		o<-optim(foo, p=theta.start, lower=lower, upper=upper, method="L")
+			
+		results<-list(lnl=-o$value, beta= exp(o$par))
+
+	#----------------------------------
+	#-----       LAMBDA ONLY      -----
+	#----------------------------------
+	} else if (model[1] & !(model[2] | model[3])){
+		
+		lower=log(lower)
+		upper=log(upper)		
+		
+		foo<-function(x) {
+
+
+			vcv<-vcv.phylo(tree)
+
+			index			<-	matrix(TRUE, n,n)
+			diag(index)		<- FALSE
+			vcv[index] 	<- vcv[index]*exp(x[2])
+			
+			vv<-exp(x[1])*vcv
+
+			
+			diag(vv)<-diag(vv)+meserr^2
+			mu<-phylogMean(vv, y)
+			mu<-rep(mu, n)
+			
+			-dmvnorm(y, mu, vv, log=T)
+		}
+		o<-optim(foo, p=theta.start, lower=lower, upper=upper, method="L")
+			
+		results<-list(lnl=-o$value, beta= exp(o$par[1]), lambda=exp(o$par[2]))
+
+	#----------------------------------
+	#-----        KAPPA ONLY      -----
+	#----------------------------------
+	} else if (model[2] & !(model[1] | model[3])){
+		
+		lower=log(lower)
+		upper=log(upper)
+				
+		
+		foo<-function(x) {
+
+			t<-kappaTree(tree, kappa=exp(x[2]))
+			vcv<-vcv.phylo(t)
+
+			
+			vv<-exp(x[1])*vcv
+
+			
+			diag(vv)<-diag(vv)+meserr^2
+			mu<-phylogMean(vv, y)
+			mu<-rep(mu, n)
+			
+			-dmvnorm(y, mu, vv, log=T)
+		}
+		o<-optim(foo, p=theta.start, lower=lower, upper=upper, method="L")
+		
+		results<-list(lnl=-o$value, beta= exp(o$par[1]), lambda=exp(o$par[2]))
+
+
+	#----------------------------------
+	#-----        DELTA ONLY      -----
+	#----------------------------------	
+	} else if (model[3] & !(model[1] | model[2])){
+		lower=log(lower)
+		upper=log(upper)
+		foo<-function(x) {
+
+			t<-deltaTree(tree, delta=exp(x[2]))
+			vcv<-vcv.phylo(t)
+
+			
+			vv<-exp(x[1])*vcv
+
+			
+			diag(vv)<-diag(vv)+meserr^2
+			mu<-phylogMean(vv, y)
+			mu<-rep(mu, n)
+			
+			-dmvnorm(y, mu, vv, log=T)
+		}
+		o<-optim(foo, p=theta.start, lower=lower, upper=upper, method="L")
+		
+		results<-list(lnl=-o$value, beta= exp(o$par[1]), delta=exp(o$par[2]))	
+		
+	#----------------------------------
+	#-----        ALPHA ONLY      -----
+	#----------------------------------			
+	} else if (model[4] & !(model[1] | model[2] | model[3])){
+		
+		lower=log(lower)
+		upper=log(upper)
+		
+		foo<-function(x) {
+			t<-ouTree(tree, exp(x[2]))
+
+			vcv<-vcv.phylo(t)
+			
+			vv<-exp(x[1])*vcv
+			diag(vv)<-diag(vv)+meserr^2
+			
+			mu<-phylogMean(vv, y)
+			mu<-rep(mu, n)
+			
+			-dmvnorm(y, mu, vv, log=T)
+		}
+		o<-optim(foo, p=theta.start, lower=lower, upper=upper, method="L")
+			
+		results<-list(lnl=-o$value, beta= exp(o$par[1]), alpha=exp(o$par[2]))
+
+
+	#----------------------------------
+	#-----        EXPON ONLY      -----
+	#----------------------------------	
+	} else if(model[5]&!(model[1] | model[2] | model[3] | model[4])){
+
+		lower[1]=log(lower[1])
+		upper[1]=log(upper[1])
+		foo<-function(x) {
+			t<-exponentialchangeTree(tree, a=x[2])
+
+			vcv<-vcv.phylo(t)
+			
+			vv<-exp(x[1])*vcv
+			diag(vv)<-diag(vv)+meserr^2
+			
+			mu<-phylogMean(vv, y)
+			mu<-rep(mu, n)
+			
+			-dmvnorm(y, mu, vv, log=T)
+		}
+		o<-optim(foo, p=theta.start, lower=lower, upper=upper, method="L")
+			
+		results<-list(lnl=-o$value, beta= exp(o$par[1]), a=o$par[2])	}else{
+		stop("Parameters  \"lambda, \"kappa\" and \"delta\" can only be fit one at a time currently")
+	}
+	
+	k=np+1
+	results$aic<-2*k-2*results$lnl
+	results$aicc<-2*k*(n-1)/(n-k-2)-2*results$lnl
+
+	return(results) 
+
+}
+
+
+
+phylogMean<-function(phyvcv, data) 
+{
+	o<-rep(1, length(data))
+	ci<-solve(phyvcv)
+	
+	m1<-solve(t(o) %*% ci %*% o)
+	m2<-t(o) %*% ci %*% data
+	
+	return(m1 %*% m2)
+	
+	}
