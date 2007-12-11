1ba1771a7386c28e9817caceb83397a15a872693
bbolker (Ben  Bolker) <bolker@mcmaster.ca>
2007-12-11 17:32:48 +0000

    vignette updates; nTips hacks
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/phyloc@51 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/Rpkgs/phylo4/R/phylo4.R
+++ trunk/Rpkgs/phylo4/R/phylo4.R
@@ -7,7 +7,11 @@ setClass("phylo4",
                         Nnode="integer",
                         tip.label="character",
                         root.edge="integer"),
-##         prototype=
+         prototype=list(edge=matrix(nrow=0,ncol=2),
+           edge.length=numeric(0),
+           Nnode=as.integer(0),
+           tip.label=character(0),
+           root.edge=as.integer(NA)),
          validity=function(object) {
            ## browser()
            N <- nrow(object@edge)
@@ -27,6 +31,15 @@ setMethod("nTips","phylo4", function(x) {
   length(x@tip.label)
 })
 
+## hack to ensure ape compatibility
+setMethod("nTips","ANY", function(x) {
+  if (class(x)=="phylo") {
+    ape::nTips(x)
+  } else stop(paste("no 'nTips' method available for",
+                    deparse(substitute(x)),
+                    "(class",class(x),")"))
+})
+
 setGeneric("nNodes", function(x) {
   standardGeneric("nNodes")
 })
@@ -34,23 +47,48 @@ setMethod("nNodes","phylo4", function(x) {
   x@Nnode
 })
 
+setGeneric("nEdges", function(x) {
+  standardGeneric("nEdges")
+})
+setMethod("nEdges","phylo4", function(x) {
+  nrow(x@edge)
+})
+
 setGeneric("edges", function(x,order,...) {
   standardGeneric("edges")
 })
 setMethod("edges","phylo4", function(x,order,...) {
-  x@edges
+  x@edge
+})
+
+setGeneric("RootEdge", function(x,order,...) {
+  standardGeneric("RootEdge")
+})
+setMethod("RootEdge","phylo4", function(x,order,...) {
+  x@root.edge
 })
 
 setGeneric("isRooted", function(x) {
   standardGeneric("isRooted")
 })
+
+##  trace("isRooted",browser)
 setMethod("isRooted","phylo4", function(x) {
-  !is.na(phy$root.edge) ||
-  tabulate(edges(phy)[, 1])[nTips(phy)+1] <= 2
+  browser()
+  !is.na(x@root.edge) ||  ## root edge explicitly defined
+  ## HACK: make sure we find the right "nTips"
+  tabulate(edges(x)[, 1])[phylo4::nTips(x)+1] <= 2
+  ## root node (first node after last tip) has <= 2 descendants
 })
 
+setGeneric("hasBranchLengths", function(x) {
+  standardGeneric("hasBranchLengths")
+})
+setMethod("hasBranchLengths","phylo4", function(x) {
+  length(x@edge.length)>0
+})
 
-## labels exists already
+## labels exists already as a generic
 setMethod("labels","phylo4", function(object,...) {
   object@tip.label
 })
@@ -89,11 +127,11 @@ as.phylo.phylo4 <- function(x) {
                 
 ## hack to allow access with $
 setMethod("$","phylo4",function(x,name) {
-  attr(x,name)
+  switch(name,
+         edge.length=if(!hasBranchLengths(x)) NULL else x@edge.length,
+         root.edge=if(is.na(x@root.edge)) NULL else x@root.edge,
+         attr(x,name))
 })
-
-
-
 
 printphylo <- function (x,printlen=6,...) {
     nb.tip <- length(x$tip.label)
@@ -114,11 +152,8 @@ printphylo <- function (x,printlen=6,...) {
         }
         else print(x$node.label)
     }
-            ## HACK 2: not handling rooting properly yet.
-            ##     rlab <- if (is.rooted(x)) 
-            ##         "Rooted"
-            ##     else "Unrooted"
-            ##     cat("\n", rlab, "; ", sep = "")
+    rlab <- if (isRooted(x)) "Rooted"  else "Unrooted"
+    cat("\n", rlab, "; ", sep = "")
     blen <- if (is.null(x$edge.length)) 
         "no branch lengths."
     else "includes branch lengths."
@@ -157,7 +192,8 @@ setMethod("summary","phylo4",
 
 ## extend: phylo with data
 setClass("phylo4d",
-         representation(data="data.frame"),
+         representation(nodedata="data.frame",
+                        edgedata="data.frame"),
          contains="phylo4")
                        
 ## extend: phylo with model fit
