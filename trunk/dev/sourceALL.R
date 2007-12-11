bababc10977a4c697c31b0d7f9752befcdfd348f
emmanuel (Dr. ) <Emmanuel.Paradis@ird.fr>
2007-12-11 17:24:01 +0000

    modified to avoid infinite loop
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/phyloc@49 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/dev/sourceALL.R
+++ trunk/dev/sourceALL.R
@@ -1,5 +1,6 @@
-d <- dir()
-Rsrc <- d[grep("\\.R$", d)]
-cat("start sourcing R files...")
+d <- dir() # get all files in dev/
+Rsrc <- d[grep("\\.R$", d)] # select only *.R files
+Rsrc <- Rsrc[-which(Rsrc == "sourceALL.R")] # remove the current file ;)
+cat("start sourcing R files...\n\n")
 for (x in Rsrc) try(source(x))
-cat("done\n\n")
+cat("Done\n")
