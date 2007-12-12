f6377b607f599769b9d7aa1011a6b1c199aa9311
damdam81 (M ) <damien.de-vienne@u-psud.fr>
2007-12-12 20:03:50 +0000

    bug fixed node names OK tip names OK
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/phyloc@99 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/dev/subtrees.R
+++ trunk/dev/subtrees.R
@@ -2,13 +2,11 @@ subtrees<-function(tree)
 {
 N.tip<-Ntip (tree)
 N.node<-Nnode(tree)
-#if (is.unrooted(tree))
-  limit<-N.tip+N.node
-print("limit")
-print(limit)
+limit<-N.tip+N.node
 sub<-list(N.node)
 u<-0
-  for (k in (N.tip+2):limit)
+
+  for (k in (N.tip+1):limit)
   {
   u<-u+1
   fils<-NULL
@@ -21,14 +19,22 @@ u<-0
 	fils<-NULL
 	if (length(pere)==0) break
 	}
+
   len<-res[res>N.tip] 
+   if (u==1) {
+	tree2<-tree
+	len<-(N.tip+1):limit
+	}
+   else {	
   len.tip<-res[res<N.tip+1] 
   vec<-1:length(tree$tip.label)
   len.tip.stay<-setdiff(vec, len.tip)
   tree2<-drop.tip(tree, len.tip.stay)
+	  }
   sub[[u]]<-tree2
-
   sub[[u]]$name<-k
+  if (is.null(tree$node.label))
+  	sub[[u]]$node.label<-len
 
   }
 return(sub)
