11ec9b7164bef1137e560766dc357ed8101c0527
skembel (Dr. ) <steve.kembel@gmail.com>
2007-12-13 21:54:08 +0000

    phylo4 subset working, took out mrca and node subset pruning for now.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/phyloc@176 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/Rpkgs/phylo4/R/subset.phylo4.R
+++ trunk/Rpkgs/phylo4/R/subset.phylo4.R
@@ -3,35 +3,28 @@
 ################
 #
 setMethod("subset", "phylo4",
-  function(x,tip.include=NULL,tips.exclude=NULL,node.subtree=NULL,...) {
+  function(x,tips.include=NULL,tips.exclude=NULL,node.subtree=NULL,...) {
     if (!is.null(tips.include)) {
-      return(prune(x,x@tip.label[-tips]))
+      if (is.numeric(tips.include)) {
+        tips.include <- x@tip.label[tips.include]
+      }
+      return(prune(x,t4@tip.label[-na.omit(match(t4@tip.label,tips.include))]))
     }
     
     if (!is.null(tips.exclude)) {
-      return(prune(x,tips))
+      return(prune(x,tips.exclude))
     }
     
-    if (!is.null(node.subtree)) {	
+    if (!is.null(node.subtree)) {
       #code from David Orme's clade.members function in CAIC
       if(length(node.subtree>1)) {
         warning(">1 node number supplied - only the first will be used")
         node.subtree <- node.subtree[1]
       }
-      if(!(node.subtree %in% 1:nNodes(x))) {
+      if(!(node.subtree %in% 1:(phylo4::nTips(x)+nNodes(x)))) {
         stop("Node number supplied not present in phylogeny")
       }
-      while(length(intN <- x[x>phylo4::nTips(x)]) > 0){		
-        minIntN <- min(intN)
-        childOfMinIntN <- with(phy, edge[,2][which(edge[,1]==minIntN)])
-        x <- c(x[x != minIntN], childOfMinIntN)
-      }
-      prune(x,x@tip.label[-unique(x)])
-    }
-    
+
     return(x)
 
 })
-
-
-
