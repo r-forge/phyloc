80b122476bb454905f1ad5534333962aff4b40e7
skembel (Dr. ) <steve.kembel@gmail.com>
2007-12-13 19:56:59 +0000

    subset generic method for subsetting phylo4 objects (pruning)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/phyloc@161 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ trunk/Rpkgs/phylo4/R/subset.phylo4.R
@@ -0,0 +1,35 @@
+################
+# subset phylo4
+################
+#
+setGeneric("subset", "phylo4",
+  function(object,tips.include=NULL,tips.exclude=NULL,node.subtree=NULL){
+
+if (!is.null(tips.include)) {
+  return(prune(object,object@tip.label[-tips]))
+}
+
+if (!is.null(tips.exclude)) {
+  return(prune(object,tips))
+}
+
+if (!is.null(node.subtree)) {	
+  #code from David Orme's clade.members function in CAIC
+  if(length(node.subtree>1)) {
+    warning(">1 node number supplied - only the first will be used")
+    node.subtree <- node.subtree[1]
+  }
+  if(!(node.subtree %in% 1:nNodes(object))) {
+    stop("Node number supplied not present in phylogeny")
+  }
+	while(length(intN <- x[x>phylo4::nTips(object)]) > 0){		
+		minIntN <- min(intN)
+		childOfMinIntN <- with(phy, edge[,2][which(edge[,1]==minIntN)])
+		x <- c(x[x != minIntN], childOfMinIntN)
+	}
+  prune(object,object@tip.label[-unique(x)])
+}
+
+return(object)
+
+})
