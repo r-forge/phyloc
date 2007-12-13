47f669d2193da4d9a7c7f6eea53129805e320d17
skembel (Dr. ) <steve.kembel@gmail.com>
2007-12-13 20:22:38 +0000

    Fixing subset method to be non generic again again x2
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/phyloc@169 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/Rpkgs/phylo4/R/subset.phylo4.R
+++ trunk/Rpkgs/phylo4/R/subset.phylo4.R
@@ -3,36 +3,12 @@
 ################
 #
 
-setGeneric("subset",signature(...))
+setGeneric("subset")
 
 setMethod("subset", "phylo4",
   function(object,tips.include=NULL,tips.exclude=NULL,node.subtree=NULL){
-
-if (!is.null(tips.include)) {
-  return(prune(object,object@tip.label[-tips]))
-}
-
-if (!is.null(tips.exclude)) {
-  return(prune(object,tips))
-}
-
-if (!is.null(node.subtree)) {	
-  #code from David Orme's clade.members function in CAIC
-  if(length(node.subtree>1)) {
-    warning(">1 node number supplied - only the first will be used")
-    node.subtree <- node.subtree[1]
-  }
-  if(!(node.subtree %in% 1:nNodes(object))) {
-    stop("Node number supplied not present in phylogeny")
-  }
-	while(length(intN <- object[object>phylo4::nTips(object)]) > 0){		
-		minIntN <- min(intN)
-		childOfMinIntN <- with(phy, edge[,2][which(edge[,1]==minIntN)])
-		object <- c(object[object != minIntN], childOfMinIntN)
-	}
-  prune(object,object@tip.label[-unique(object)])
-}
-
-return(object)
-
+  warning("Hello")
 })
+
+
+
