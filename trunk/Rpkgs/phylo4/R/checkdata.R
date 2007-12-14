f5b4f3fdf16c08f4392154e397789c0ff3a67b74
skembel (Dr. ) <steve.kembel@gmail.com>
2007-12-14 02:48:00 +0000

    Fixing data attach to phylo4d
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/phyloc@197 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- trunk/Rpkgs/phylo4/R/checkdata.R
+++ trunk/Rpkgs/phylo4/R/checkdata.R
@@ -203,7 +203,7 @@ attach_data <- function(object,
 	if (!all(dim(object@tip.data)==0)) {
 		## if we want to use tip.names
 		if (use.tip.names) {
-			object@tip.data <- object@tip.data[match(row.names(object@tip.data),object@tip.label),]
+			object@tip.data <- object@tip.data[match(object@tip.label,row.names(object@tip.data)),]
 			row.names(object@tip.data) <- object@tip.label
 		}
 	}
@@ -212,7 +212,7 @@ attach_data <- function(object,
 	if (!all(dim(object@node.data)==0)) {
 		## if we want to use tip.names
 		if (use.node.names) {
-			object@node.data <- object@node.data[match(row.names(object@node.data),object@node.label),]
+			object@node.data <- object@node.data[match(object@node.label,row.names(object@node.data)),]
 			row.names(object@node.data) <- object@node.label
 		}
 	}
